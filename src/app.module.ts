e04f8ca70e2f486068ac5079a12b10dc37652c92
anouar garbaa <g.anouar@yahoo.com>
2025-03-30 23:48:41 +0200

    generate chat module with nested module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 
-@Module({
-  imports: [],
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { ChatModule } from './chat/chat.module';
 
+
+@Module({
+  imports: [UsersModule, ProductsModule, ChatModule],
+  controllers: [],
+  providers: [],
+  exports: []
+  
 })
 export class AppModule {}
