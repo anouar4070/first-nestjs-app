337a66ebd5afa9b41cac61411a7252dd9d43fe37
anouar garbaa <g.anouar@yahoo.com>
2025-04-02 17:57:34 +0200

    Modules Re-Exporting

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,6 +2,7 @@ import { MessagesModule } from './../messages/messages.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [MessagesModule]
+  imports: [MessagesModule],
+  exports: [MessagesModule],
 })
 export class ChatModule {}
