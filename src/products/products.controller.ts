5dee3d0c344f15eed57fdd79d4f00ce3a515a40e
anouar garbaa <g.anouar@yahoo.com>
2025-04-03 19:36:56 +0200

    req Params

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('products') 
 export class ProductsController {
+ private products: {id: number, title: string}[] = [
+  {id: 1, title: 'First Product'},
+  {id: 2, title: 'Second Product'},
+  {id: 3, title: 'Third Product'},
+];
+
   @Get() // ** /products
   getProducts() {
-    return [{id: 1, title: 'First Product'}];
+    return this.products;
   }
 
+  @Get(':id') // ** /products/:id
+  getProductsById(@Req() req: Request) {
+    console.log(req.params)
+    return this.products.filter(product => product.id === +req.params.id) ;
+  }
 }
 
