12a1db06f3aac17fc63f243c8a8d9bea95f04663
anouar garbaa <g.anouar@yahoo.com>
2025-04-03 19:51:29 +0200

    @Param Decorator

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Param, Req } from '@nestjs/common';
+//import { Request } from 'express';
 
 @Controller('products') 
 export class ProductsController {
@@ -15,9 +15,17 @@ export class ProductsController {
   }
 
   @Get(':id') // ** /products/:id
-  getProductsById(@Req() req: Request) {
-    console.log(req.params)
-    return this.products.filter(product => product.id === +req.params.id) ;
+  // getProductsById(@Req() req: Request) {
+  //   console.log(req.params)
+  //   return this.products.filter(product => product.id === +req.params.id) ;
+  // }
+  getProductsById(@Param('id') id: string) {
+    const filteredProducts = this.products.filter(product => product.id === +id) ;
+    if (filteredProducts.length) {
+      return filteredProducts
+    }
+    
+    return "Product not found"
   }
 }
 
