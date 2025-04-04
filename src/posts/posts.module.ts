3da93f67e2721e859480090a85b4698bb41201f4
anouar garbaa <g.anouar@yahoo.com>
2025-04-05 00:09:27 +0200

    Applying Posts middleware

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,34 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { PostsMiddleware } from './posts.middleware';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
 })
-export class PostsModule {}
+export class PostsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //consumer.apply(PostsMiddleware).forRoutes('posts')
+    //consumer.apply(PostsMiddleware).forRoutes({path: "posts", method: RequestMethod.GET});
+    //consumer.apply(PostsMiddleware).exclude('posts');
+    consumer
+      .apply(PostsMiddleware)
+      .exclude(
+        {
+          path: 'posts/:id',
+          method: RequestMethod.GET,
+        },
+        {
+          path: 'posts/:id',
+          method: RequestMethod.DELETE,
+        },
+      )
+      .forRoutes('posts');
+  }
+}
