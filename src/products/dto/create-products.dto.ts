61403128c53e2d6ed26f006655919db5353ace87
anouar garbaa <g.anouar@yahoo.com>
2025-04-04 15:59:53 +0200

    Validate Nested Object

--- src/products/dto/create-products.dto.ts
+++ src/products/dto/create-products.dto.ts
@@ -1,11 +1,34 @@
-import {IsNotEmpty, Length, Max} from 'class-validator'
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  Length,
+  Max,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
+class CreateProductDetailsDto {
+  @IsNotEmpty()
+  color: string;
+  @IsNotEmpty()
+  @IsInt()
+  size: string;
+  @IsNotEmpty()
+  brand: string;
+}
 export class CreateProductDto {
-   // ** REQUIRED
+  // ** REQUIRED
 
   @Length(5, 50)
   title: string;
+
   @IsNotEmpty()
-  @Max(50)  //using "@Max()" decorator, "description" become now required
+  @Max(50) //using "@Max()" decorator, "description" become now required
   description: string;
+
+  @ValidateNested()
+  @IsNotEmptyObject()
+  @Type(() => CreateProductDetailsDto)
+  details: CreateProductDetailsDto;
 }
